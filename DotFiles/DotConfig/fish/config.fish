8705ee8355896bf91ad0054aeb31d5e9c3cbde2e
ir <kakuzamu0718874@gmail.com>
2019-12-14 01:42:02 +0000

    update fish prompt

--- DotFiles/DotConfig/fish/config.fish
+++ DotFiles/DotConfig/fish/config.fish
@@ -36,6 +36,36 @@ if [ -e $HOME/.localrc ]; source $HOME/.localrc; end
 function fish_greeting
 end
 
+# [dev: nomodify]
+function git_status_prompt
+    set -l git_branch (git branch 2> /dev/null | grep -e '\*' | sed 's/^..\(.*\)/\1/')
+    set -l git_diff (git diff)
+    set -l git_diff_cached (git diff --cached)
+    set_color red
+    if test -n "$git_diff"
+        echo [(set_color red){$git_branch}: (set_color yellow)"dirty"(set_color red)]
+    else if test -n "$git_diff_cached"
+        echo [(set_color red){$git_branch}: (set_color green)"staged"(set_color red)]
+    else
+        echo [(set_color red){$git_branch}: (set_color brgreen)"nochange"(set_color red)]
+    end
+end
+
+function fish_prompt
+    set -l exit_status $status
+    set -l is_git_repo (git rev-parse --git-dir 2> /dev/null)
+    if test -n "$is_git_repo"
+        printf "%s:%s %s" (set_color cyan)$USER (set_color green)(prompt_pwd) (git_status_prompt)
+    else
+        printf "%s:%s" (set_color cyan)$USER (set_color green)(prompt_pwd)
+    end
+    if test $exit_status -eq 0
+        printf "\n%s " (set_color normal)"->"
+    else
+        printf "\n%s " (set_color red)"->"
+    end
+end
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
