eb768d9f819293b23da13ab90cbf2e6305392481
ir <kakuzamu0718874@gmail.com>
2020-05-25 01:02:58 +0900

    update setup script for MSYS2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$OS" == "Windows_NT" ]; then
+    export MSYS='winsymlinks:nativestrict'
+    mkdir -p /c/Users/$USER/AppData/Roaming/Hyper
+    ln -sfnv $PWD/DotFiles/DotConfig/Hyper/.hyper.js /c/Users/$USER/AppData/Roaming/Hyper/.hyper.js
+fi
+
 for dotfile in DotFiles/*;do
     if [ $dotfile = DotFiles/DotConfig ]; then continue; fi
     ln -sfnv $PWD/$dotfile $HOME/.${dotfile##*/}
