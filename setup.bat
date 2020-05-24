7a56f47b8fa93f193e0005fbb2f972a46a4e8a5f
ir <kakuzamu0718874@gmail.com>
2020-05-25 00:58:58 +0900

    stop development with powershell

--- setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Mklink /D %USERPROFILE%\vimfiles "%~dp0"DotFiles\vim
-Mklink %USERPROFILE%\_vimrc "%~dp0"DotFiles\vimrc
-Mklink %USERPROFILE%\_gvimrc "%~dp0"DotFiles\gvimrc
-Mklink %USERPROFILE%\.gitconfig "%~dp0"DotFiles\gitconfig
-Mklink %USERPROFILE%\Documents\WindowsPowerShell\Profile.ps1 "%~dp0"WinCfg\Profile.ps1
