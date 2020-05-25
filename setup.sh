4ca01f3a3c24df41c649a34e8855cdeb46a18d54
ir <kakuzamu0718874@gmail.com>
2020-05-25 20:36:30 +0900

    move Hyper Terminal setting

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$OS" == "Windows_NT" ]; then
-    export MSYS='winsymlinks:nativestrict'
-    mkdir -p /c/Users/$USER/AppData/Roaming/Hyper
-    ln -sfnv $PWD/DotFiles/DotConfig/Hyper/.hyper.js /c/Users/$USER/AppData/Roaming/Hyper/.hyper.js
-fi
-
 for dotfile in DotFiles/*;do
     if [ $dotfile = DotFiles/DotConfig ]; then continue; fi
     ln -sfnv $PWD/$dotfile $HOME/.${dotfile##*/}
@@ -15,3 +9,12 @@ mkdir -p $HOME/.config
 for dotconfigfile in DotFiles/DotConfig/*;do
     ln -sfnv $PWD/$dotconfigfile $HOME/.config/${dotconfigfile##*/}
 done
+
+# create hyper symbolic link
+if [ "$OS" == "Windows_NT" ]; then
+    export MSYS='winsymlinks:nativestrict'
+    mkdir -p /c/Users/$USER/AppData/Roaming/Hyper
+    ln -sfnv $PWD/hyper/hyper_msys2.js /c/Users/$USER/AppData/Roaming/Hyper/.hyper.js
+else
+    ln -sfnv $PWD/hyper/hyper_unix.js $HOME/.hyper.js
+fi
