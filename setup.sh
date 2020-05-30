c7e58a7f4cb0a0e17b689d45cbd07ad703ab705b
ir <kakuzamu0718874@gmail.com>
2020-05-30 13:53:02 +0900

    remove hyper terminal settings

--- setup.sh
+++ setup.sh
@@ -9,12 +9,3 @@ mkdir -p $HOME/.config
 for dotconfigfile in DotFiles/DotConfig/*;do
     ln -sfnv $PWD/$dotconfigfile $HOME/.config/${dotconfigfile##*/}
 done
-
-# create hyper symbolic link
-if [ "$OS" == "Windows_NT" ]; then
-    export MSYS='winsymlinks:nativestrict'
-    mkdir -p /c/Users/$USER/AppData/Roaming/Hyper
-    ln -sfnv $PWD/hyper/hyper_msys2.js /c/Users/$USER/AppData/Roaming/Hyper/.hyper.js
-else
-    ln -sfnv $PWD/hyper/hyper_unix.js $HOME/.hyper.js
-fi
