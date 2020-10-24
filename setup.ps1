fc72c3057d549c41f16a5766143bb171951915fe
ir <kakuzamu0718874@gmail.com>
2020-10-25 02:07:27 +0900

    add setup script for windows

---
+++ setup.ps1
@@ -0,0 +1,10 @@
+# hide error message  when file exist
+$ErrorActionPreference = "silentlycontinue"
+
+# create symboliclink of vim
+New-Item -Value $PWD\DotFiles\vimrc -Path $HOME -Name _vimrc -ItemType SymbolicLink
+New-Item -Value $PWD\DotFiles\gvimrc -Path $HOME -Name _gvimrc -ItemType SymbolicLink
+New-Item -Value $PWD\DotFiles\vim -Path $HOME -Name vimfiles -ItemType SymbolicLink
+
+# create symboliclink of git
+New-Item -Value $PWD\DotFiles\gitconfig -Path $HOME -Name .gitconfig -ItemType SymbolicLink
