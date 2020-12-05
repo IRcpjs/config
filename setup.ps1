d33b08f555c6d24677e960779cc5f6b70626ec81
ir <kakuzamu0718874@gmail.com>
2020-12-05 17:45:26 +0900

    change directory

--- setup.ps1
+++ setup.ps1
@@ -2,12 +2,12 @@
 $ErrorActionPreference = "silentlycontinue"
 
 # create symboliclink of vim
-New-Item -Value $PWD\DotFiles\vimrc -Path $HOME -Name _vimrc -ItemType SymbolicLink
-New-Item -Value $PWD\DotFiles\gvimrc -Path $HOME -Name _gvimrc -ItemType SymbolicLink
-New-Item -Value $PWD\DotFiles\vim -Path $HOME -Name vimfiles -ItemType SymbolicLink
+New-Item -Value $PWD\dotfiles\vimrc -Path $HOME -Name _vimrc -ItemType SymbolicLink
+New-Item -Value $PWD\dotfiles\gvimrc -Path $HOME -Name _gvimrc -ItemType SymbolicLink
+New-Item -Value $PWD\dotfiles\vim -Path $HOME -Name vimfiles -ItemType SymbolicLink
 
 # create symboliclink of git
-New-Item -Value $PWD\DotFiles\gitconfig -Path $HOME -Name .gitconfig -ItemType SymbolicLink
+New-Item -Value $PWD\dotfiles\gitconfig -Path $HOME -Name .gitconfig -ItemType SymbolicLink
 
 # create symboliclink of ideavim
-New-Item -Value $PWD\DotFiles\ideavimrc -Path $HOME -Name _ideavimrc -ItemType SymbolicLink
+New-Item -Value $PWD\dotfiles\ideavimrc -Path $HOME -Name _ideavimrc -ItemType SymbolicLink
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for dotfile in DotFiles/*;do
-    if [ $dotfile = DotFiles/DotConfig ]; then continue; fi
+for dotfile in dotfiles/*;do
+    if [ $dotfile = dotfiles/dotconfig ]; then continue; fi
     ln -sfnv $PWD/$dotfile $HOME/.${dotfile##*/}
 done
 
 mkdir -p $HOME/.config
-for dotconfigfile in DotFiles/DotConfig/*;do
+for dotconfigfile in dotfiles/dotconfig/*;do
     ln -sfnv $PWD/$dotconfigfile $HOME/.config/${dotconfigfile##*/}
 done
