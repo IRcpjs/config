6f5ee20b3a648edb32fa5bb5ea27e4a5059ce173
ir <kakuzamu0718874@gmail.com>
2020-12-05 19:49:27 +0900

    update for windows

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,9 @@
 # hide error message  when file exist
 $ErrorActionPreference = "silentlycontinue"
 
+# create symboliclink of powershell7
+New-Item -Value $PWD\windows\profile.ps1 -Path $HOME\Documents\WindowsPowerShell -Name profile.ps1 -ItemType SymbolicLink
+
 # create symboliclink of vim
 New-Item -Value $PWD\dotfiles\vimrc -Path $HOME -Name _vimrc -ItemType SymbolicLink
 New-Item -Value $PWD\dotfiles\gvimrc -Path $HOME -Name _gvimrc -ItemType SymbolicLink
