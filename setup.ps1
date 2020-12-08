26ae907d7925d4b837f9c0e3af8ecb4f9e8b31df
ir <kakuzamu0718874@gmail.com>
2020-12-08 22:56:37 +0900

    add windows terminal config

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,11 @@ $ErrorActionPreference = "silentlycontinue"
 mkdir $HOME\Documents\PowerShell
 New-Item -Value $PWD\windows\profile.ps1 -Path $HOME\Documents\PowerShell -Name profile.ps1 -ItemType SymbolicLink
 
+# create symboliclink of windows terminal
+$wt_dir="$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+mkdir $wt_dir
+New-Item -Value $PWD\windows\settings.json -Path $wt_dir -Name settings.json -ItemType SymbolicLink
+
 # create symboliclink of vim
 New-Item -Value $PWD\dotfiles\vimrc -Path $HOME -Name _vimrc -ItemType SymbolicLink
 New-Item -Value $PWD\dotfiles\gvimrc -Path $HOME -Name _gvimrc -ItemType SymbolicLink
