c03bcfd1fc6c9d3e5d59c622d1861241bd0d845c
ir <kakuzamu0718874@gmail.com>
2020-12-31 20:27:26 +0900

    add homebrew package list

--- init/macos.sh
+++ init/macos.sh
@@ -2,8 +2,26 @@
 set -e
 
 brew_packages=(
+    "docker"
+    "fish"
+    "fzf"
+    "ghq"
+    "hub"
+    "jq"
+    "ranger"
+    "rclone"
+    "tmux"
+    "vim"
 )
 
+brew_cask_packages=(
+    "docker"
+    "google-backup-and-sync"
+    "google-chrome"
+    "keepassxc"
+)
+
+# install homebrew
 if !(type brew > /dev/null 2>&1); then
     echo "brew command not found. install homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,6 +29,22 @@ else
     echo "brew command exist. skip installing homebrew"
 fi
 
+# install brew packages
 for pack in ${brew_packages[@]};do
-    brew install ${pack}
+    if brew ls --version ${pack} >/dev/null; then
+        echo "${pack} is installed. skip"
+    else
+        echo "${pack} is not installed. install ${pack}"
+        brew install ${pack}
+    fi
+done
+
+# install brew cask packages
+for pack in ${brew_cask_packages[@]};do
+    if brew ls --cask --version ${pack} >/dev/null; then
+        echo "${pack} is installed. skip"
+    else
+        echo "${pack} is not installed. install ${pack}"
+        brew install --cask ${pack}
+    fi
 done
